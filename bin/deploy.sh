e2165c854e8ed90ac180671f517b5b740a848f77
pdev <pjv99dev@gmail.com>
2023-09-20 15:14:39 -0400

    Adding in new bash profile source to see if this solves the trick

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+source /home/ec2-user/.bash_profile
 
 cd /app
 
