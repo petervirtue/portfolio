309b15e4bf12038b6d84631c2691ad5ae4bea82c
pdev <pjv99dev@gmail.com>
2023-09-20 14:55:55 -0400

    Adding in some script adjustments

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 cd /app
 npm install &&
 npm build &&
-pm2 restart [name] 
+pm2 restart petervirtue
