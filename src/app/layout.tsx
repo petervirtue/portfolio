6d3c3fa95f2f96ce5a717e8e7073e5cebc63d632
pdev <pjv99dev@gmail.com>
2023-09-21 13:23:39 -0400

    Giving google analytics a spin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
+import { GA_TRACKING_ID } from "@/lib/gtag";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export const metadata: Metadata = {
   title: "Peter Virtue",
@@ -17,6 +20,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* enable analytics script only for production */}
+        {isProduction && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+              }}
+            />
+          </>
+        )}
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
