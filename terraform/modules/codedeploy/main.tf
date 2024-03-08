0739e085c9e4df906ea64fb4e08591799d5eb49d
petervirtue <petervirtue18@gmail.com>
2024-03-07 19:16:35 -0500

    Adjusting image identifier

--- terraform/modules/codedeploy/main.tf
+++ terraform/modules/codedeploy/main.tf
@@ -17,7 +17,7 @@ resource "aws_codebuild_project" "build" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "al2/standard/5.0"
+    image        = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     type         = "LINUX_CONTAINER"
   }
 
