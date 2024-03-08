0202df591fd5f1353cc3fea703e97b5ab3e33a38
petervirtue <petervirtue18@gmail.com>
2024-03-08 16:16:19 -0500

    I am new to lambda

--- terraform/modules/codedeploy/data.tf
+++ terraform/modules/codedeploy/data.tf
@@ -1,5 +1,5 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_file = "${path.module}/invalidate_cache.py"
+  source_file = "${path.module}/index.py"
   output_path = "${path.module}/lambda_function.zip"
 }
